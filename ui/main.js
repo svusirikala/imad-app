400f642429808c67293a9341da9dcc613c710982
Sri Lakshmi <SriLakshmi@Gonuguntlas-MacBook-Pro.local>
2017-08-08 18:34:46 +0700

    made changes for p5 module

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+// change the index text
+var element = document.getElementById('main-text');
+element.innerHTML = 'let\'s make our blog';
+
+// move the image
+var img = document.getElementById('sri');
+
+var marginLeft = 0;
+
+function moveRight() {
+   marginLeft = marginLeft + 5 ;
+   img.style.marginLeft = marginLeft + 'px' ;
+}
+img.onclick = function () {
+	var interval  = setInterval(moveRight, 50);
+}
