7c84dc980ad8af8d5af90c499df2b1524e1d1de8
Sri Lakshmi <SriLakshmi@Gonuguntlas-MacBook-Pro.local>
2017-08-18 14:52:39 +0700

    made changes for week2

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool ;
+
+
+var config = { 
+    user : 'svusirikala7',
+    database: 'svusirikala7',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+ //   password : process.env.DB_PASSWORD
+    password: 'db-svusirikala7-59637'
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -104,7 +115,7 @@ function createTemplate (data) {
 	              ${heading}
 	          </h3>
 	          <div>
-	              ${date}
+	             ${date}
 	          </div>
 	          <div>
 	              ${content}
@@ -120,13 +131,47 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
+var pool = new Pool(config);
+app.get('/test-db', function(req,res) { 
+	pool.query('SELECT *  FROM test', function(err,result) {
+   		if (err)  {
+   			res.status(500).send(err.toString());
+   		} else {
+   			res.send(JSON.stringify(result.rows));
+   		}
+   	});
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1;
+  res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function(req,res) {
+  var name = req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
+app.get('/articles/:articleName', function (req, res) {
 	// articleName is the variable that will loop thru 
 	// all the objects in the article object
   var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+  pool.query("SELECT *  FROM articles WHERE title = $1",[req.params.articleName], function(err,result) {
+   		if (err)  {
+   			res.status(500).send(err.toString());
+   		} else {
+   			if (result.rows.length === 0) {
+   				res.status(404).send('Article not found');
+   			} else {
+   				var articleData = result.rows[0];
+   				res.send(createTemplate(articleData));
+   			}
+   		}
+   	});
 });
-
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
